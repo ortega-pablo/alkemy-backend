6d073153fc72836c3869e48368af7b4ca48a3b2f
ortega-pablo <p.ortegariera@gmail.com>
2022-06-29 23:53:44 -0300

    agrego algunas rutas de movies

--- api/src/routes/characters.js
+++ api/src/routes/characters.js
@@ -1,250 +1,241 @@
 const { Router } = require("express");
 const router = Router();
-const verifyToken = require ("../middlewares/auth")
-const {Character, Movie} = require("../db")
+const verifyToken = require("../middlewares/auth");
+const { Character, Movie } = require("../db");
 
+router.get("/", verifyToken, async (req, res, next) => {
+  try {
+    const { name, age, movies } = req.query;
 
-router.get("/", verifyToken, async (req,res,next) =>{
-
-    try {
-        
-        const {name, age, movieId } = req.query
-
-        if(!name && !age && !movieId){
-            const allCharacters = await Character.findAll({
-                attributes: ["id", "name", "image"],
-            })
-            res.status(200).send(allCharacters)
-        }
-
-        const allCharacters = await Character.findAll({
-            attributes: ["id", "name", "image", "age", "weight", "history"],
-        })
-        if(name){
-            console.log("Tengo nombre")
-            let charactersByName = allCharacters.filter(c => c.name.toLowerCase().includes(name.toLowerCase()))
-            if(charactersByName.length>0){
-                let orderedCharacters = await charactersByName.sort((a, b) => {
-                    if (a.name.length > b.name.length) {
-                      return 1;
-                    } else if (a.name.length < b.name.length) {
-                      return -1;
-                    } else {
-                      return 0;
-                    }
-                  });
-                
-                res.status(200).send(orderedCharacters)
-            } else {
-                res.status(400).send("No se encontraron coincidencias")
-            }
-        }
-
-        if(age){
-            console.log("Tengo age")
-            let charactersByAge = allCharacters.filter(c => c.age == age)
-            
-            if(charactersByAge.length>0){
-                return res.status(200).send(charactersByAge)
-            }else{
-                return res.status(400).send("No se encontraron personajes con la edad solicitada")
-            }
-        }
-
-        if(movieId){
-            let charactersByMovie = await Character.findAll({
-                where:{
-                    include: [
-                        {
-                          model: Movie,
-                          attributes: ["image", "title", "createdDate", "rating"],
-                        },
-                    ]
-                }
-            })
-            if(charactersByMovie){
-                return res.status(200).send(charactersByMovie)
-            }else{
-                return res.status(400).send("No se encontraron personajes asociados a la película seleccionada")
-            }
-        }
-
-
-    } catch (error) {
-        next(error)
+    if (!name && !age && !movies) {
+      const allCharacters = await Character.findAll({
+        attributes: ["name", "image"],
+      });
+      res.status(200).send(allCharacters);
     }
-})
 
-router.post("/", verifyToken, async (req,res,next) =>{
+    const allCharacters = await Character.findAll({
+      attributes: ["id", "name", "image", "age", "weight", "history"],
+    });
+    if (name) {
+      let charactersByName = allCharacters.filter((c) =>
+        c.name.toLowerCase().includes(name.toLowerCase())
+      );
+      if (charactersByName.length > 0) {
+        let orderedCharacters = await charactersByName.sort((a, b) => {
+          if (a.name.length > b.name.length) {
+            return 1;
+          } else if (a.name.length < b.name.length) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
 
-    try {
-        
-        const {image, name, age, weight, history} = req.body
-        const newCharacter = await Character.create({
-            image,
-            name,
-            age,
-            weight,
-            history,
-        })
-
-        res.status(200).send(newCharacter)
-
-    } catch (error) {
-        next(error)
+        res.status(200).send(orderedCharacters);
+      } else {
+        res.status(400).send("No se encontraron coincidencias");
+      }
     }
-})
 
-router.put("/", verifyToken, async (req,res,next) =>{
+    if (age) {
+      let charactersByAge = allCharacters.filter((c) => c.age == age);
 
-    try {
-        
-        const {characterId, image, name, age, weight, history} = req.body
+      if (charactersByAge.length > 0) {
+        return res.status(200).send(charactersByAge);
+      } else {
+        return res
+          .status(400)
+          .send("No se encontraron personajes con la edad solicitada");
+      }
+    }
 
-        const findCharacter = await Character.findOne({
-            where:{
-                id: characterId
-            }
-        })
-
-        if(findCharacter){
-            await Character.update({
-                image,
-                name,
-                age,
-                weight,
-                history,
-            },
+    if (movies) {
+      let charactersByMovie = await Character.findAll({
+          include: [
             {
-                where:{
-                    id: characterId
-                }
-            })
-            res.status(200).send("Personaje modificado correctamente")
-        }else{
-            res.status(400).send("Personaje no encontrado.")
-        }
-
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.delete("/", verifyToken, async (req,res,next) =>{
-
-    try {
-        
-        const {characterId} = req.body
-        
-        const findCharacter = await Character.findOne({
-            where:{
-                id: characterId
-            }
-        })
-
-        if(findCharacter){
-            await Character.destroy({
-                where:{
-                    id: characterId
-                }
-            })
-            res.status(200).send("Personaje eliminado correctamente.")
-        } else {
-            res.status(400).send("Personaje no encontrado")
-        }
-
-
-
-        res.status(200).send(newCharacter)
-
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.post("/addMovie", verifyToken, async (req,res,next) =>{
-
-    try {
-        
-        const {characterId, movieId} = req.body
-
-        const findCharacter = await Character.findOne({
-            id: characterId,
-        })
-        const findMovie = await Movie.findOne({
-            id: movieId,
-        })
-
-        if(findCharacter){
-            if(findMovie){
-
-                findCharacter.addMovie(findMovie)
-        
-                res.status(200).send("Película asociada con éxito")
-            }else{
-                return res.status(400).send("Película no encontrada")
-            }
-        }else{
-            return res.status(400).send("Personaje no encontrado")
-        }
-
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.put("/removeMovie", verifyToken, async (req,res,next) =>{
-
-    try {
-        
-        const {characterId, movieId} = req.body
-
-        const findCharacter = await Character.findOne({
-            id: characterId,
-        })
-        const findMovie = await Movie.findOne({
-            id: movieId,
-        })
-
-        if(findCharacter){
-            if(findMovie){
-
-                findCharacter.removeMovie(findMovie)
-        
-                res.status(200).send("Película eliminada con éxito")
-            }else{
-                return res.status(400).send("Película no encontrada")
-            }
-        }else{
-            return res.status(400).send("Personaje no encontrado")
-        }
-
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.get("/:characterId", verifyToken, async (req,res,next) =>{
-
-    try {
-        const {characterId} = req.params
-        const characterDetail = await Character.findOne({
-            where:{
-                id: characterId
+              model: Movie,
+              where:{
+                id:movies
+              }
             },
-            include: [
-                {
-                  model: Movie,
-                  attributes: ["image", "title", "createdDate", "rating"],
-                },
-            ]
-        })
-
-        res.status(200).send(characterDetail)
-
-    } catch (error) {
-        next(error)
+          ],
+      });
+      if (charactersByMovie) {
+        return res.status(200).send(charactersByMovie);
+      } else {
+        return res
+          .status(400)
+          .send(
+            "No se encontraron personajes asociados a la película seleccionada"
+          );
+      }
     }
-})
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", verifyToken, async (req, res, next) => {
+  try {
+    const { image, name, age, weight, history } = req.body;
+    const newCharacter = await Character.create({
+      image,
+      name,
+      age,
+      weight,
+      history,
+    });
+
+    res.status(200).send(newCharacter);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/", verifyToken, async (req, res, next) => {
+  try {
+    const { characterId, image, name, age, weight, history } = req.body;
+
+    const findCharacter = await Character.findOne({
+      where: {
+        id: characterId,
+      },
+    });
+
+    if (findCharacter) {
+      await Character.update(
+        {
+          image,
+          name,
+          age,
+          weight,
+          history,
+        },
+        {
+          where: {
+            id: characterId,
+          },
+        }
+      );
+      res.status(200).send("Personaje modificado correctamente");
+    } else {
+      res.status(400).send("Personaje no encontrado.");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/", verifyToken, async (req, res, next) => {
+  try {
+    const { characterId } = req.body;
+
+    const findCharacter = await Character.findOne({
+      where: {
+        id: characterId,
+      },
+    });
+
+    if (findCharacter) {
+      await Character.destroy({
+        where: {
+          id: characterId,
+        },
+      });
+      res.status(200).send("Personaje eliminado correctamente.");
+    } else {
+      res.status(400).send("Personaje no encontrado");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/addMovie", verifyToken, async (req, res, next) => {
+  try {
+    const { characterId, movieId } = req.body;
+    console.log(characterId, movieId);
+
+    const findCharacter = await Character.findOne({
+      where: {
+        id: characterId,
+      },
+    });
+    console.log("encontre prsonaje", findCharacter);
+    const findMovie = await Movie.findOne({
+      where: {
+        id: movieId,
+      },
+    });
+    console.log("encontre pelicula", findMovie);
+
+    if (findCharacter) {
+      if (findMovie) {
+        findCharacter.addMovie(findMovie);
+
+        res.status(200).send("Película asociada con éxito");
+      } else {
+        return res.status(400).send("Película no encontrada");
+      }
+    } else {
+      return res.status(400).send("Personaje no encontrado");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/removeMovie", verifyToken, async (req, res, next) => {
+  try {
+    const { characterId, movieId } = req.body;
+
+    const findCharacter = await Character.findOne({
+      where: {
+        id: characterId,
+      },
+    });
+    const findMovie = await Movie.findOne({
+      where: {
+        id: movieId,
+      },
+    });
+
+    if (findCharacter) {
+      if (findMovie) {
+        findCharacter.removeMovie(findMovie);
+
+        res.status(200).send("Película eliminada con éxito");
+      } else {
+        return res.status(400).send("Película no encontrada");
+      }
+    } else {
+      return res.status(400).send("Personaje no encontrado");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:characterId", verifyToken, async (req, res, next) => {
+  try {
+    const { characterId } = req.params;
+    const characterDetail = await Character.findOne({
+      where: {
+        id: characterId,
+      },
+      include: [
+        {
+          model: Movie,
+          attributes: ["id", "image", "title", "createdDate", "rating"],
+        },
+      ],
+    });
+
+    res.status(200).send(characterDetail);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- api/src/routes/movies.js
+++ api/src/routes/movies.js
@@ -1,71 +1,90 @@
 const { Router } = require("express");
 const router = Router();
 const verifyToken = require ("../middlewares/auth")
-const {Character, Movie} = require("../db")
+const {Character, Movie, Genre} = require("../db")
 
 router.get("/", verifyToken, async (req,res,next) =>{
 
     try {
         
-        const {name, age, movieId } = req.query
+        const {name, idGenero, order } = req.query
 
-        if(!name && !age && !movieId){
-            const allCharacters = await Character.findAll({
-                attributes: ["id", "name", "image"],
+        if(!name && !idGenero && !order){
+            const allMovies = await Movie.findAll({
+                attributes: ["image", "title", "createdDate"],
             })
-            res.status(200).send(allCharacters)
+            res.status(200).send(allMovies)
         }
 
-        const allCharacters = await Character.findAll({
-            attributes: ["id", "name", "image", "age", "weight", "history"],
+        const allMovies = await Movie.findAll({
+            attributes: ["id", "image", "title", "createdDate", "rating"],
         })
         if(name){
-            console.log("Tengo nombre")
-            let charactersByName = allCharacters.filter(c => c.name.toLowerCase().includes(name.toLowerCase()))
-            if(charactersByName.length>0){
-                let orderedCharacters = await charactersByName.sort((a, b) => {
-                    if (a.name.length > b.name.length) {
+            let moviesByName = allMovies.filter(m => m.title.toLowerCase().includes(title.toLowerCase()))
+            if(moviesByName.length>0){
+                let orderedMovies = await moviesByName.sort((a, b) => {
+                    if (a.title.length > b.title.length) {
                       return 1;
-                    } else if (a.name.length < b.name.length) {
+                    } else if (a.title.length < b.title.length) {
                       return -1;
                     } else {
                       return 0;
                     }
                   });
                 
-                res.status(200).send(orderedCharacters)
+                res.status(200).send(orderedMovies)
             } else {
                 res.status(400).send("No se encontraron coincidencias")
             }
         }
 
-        if(age){
-            console.log("Tengo age")
-            let charactersByAge = allCharacters.filter(c => c.age == age)
-            
-            if(charactersByAge.length>0){
-                return res.status(200).send(charactersByAge)
-            }else{
-                return res.status(400).send("No se encontraron personajes con la edad solicitada")
-            }
-        }
-
-        if(movieId){
-            let charactersByMovie = await Character.findAll({
+        if(idGenero){
+            let moviesByGenre = await Movie.findAll({
                 where:{
                     include: [
                         {
-                          model: Movie,
-                          attributes: ["image", "title", "createdDate", "rating"],
+                          model: Genre,
+                          attributes: ["image", "name"],
                         },
                     ]
                 }
             })
-            if(charactersByMovie){
-                return res.status(200).send(charactersByMovie)
+            if(moviesByGenre){
+                return res.status(200).send(moviesByGenre)
             }else{
-                return res.status(400).send("No se encontraron personajes asociados a la película seleccionada")
+                return res.status(400).send("No se encontraron Películas o series asociadas al género seleccionado")
             }
+        }
+
+        if(order){
+            if(order === "ASC"){
+                let orderedMovies = await allMovies.sort((a, b) => {
+                    if (a > b) {
+                      return 1;
+                    } else if (a < b) {
+                      return -1;
+                    } else {
+                      return 0;
+                    }
+                  });
+                
+                res.status(200).send(orderedMovies)
+            } else if(order === "DESC"){
+                let orderedMovies = await allMovies.sort((a, b) => {
+                    if (a > b) {
+                      return -1;
+                    } else if (a < b) {
+                      return 1;
+                    } else {
+                      return 0;
+                    }
+                  });
+                
+                res.status(200).send(orderedMovies)
+            }else {
+                res.status(400).send("Error al ordenar las películas")
+            }
+
         }
 
 
@@ -73,6 +92,116 @@ router.get("/", verifyToken, async (req,res,next) =>{
         next(error)
     }
 })
+
+router.get("/:movieId", verifyToken, async (req,res,next) =>{
+
+    try {
+        const {movieId} = req.params
+        const movieDetail = await Movie.findOne({
+            where:{
+                id: movieId
+            },
+            include: [
+                {
+                  model: Character,
+                  attributes: ["image", "name", "age", "weight", "history"],
+                },
+                {
+                    model: Genre,
+                    attributes: ["name", "image"],
+                  },
+            ]
+        })
+
+        res.status(200).send(movieDetail)
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post("/", verifyToken, async (req,res,next) =>{
+
+    try {
+        
+        const {image, title, createdDate, rating} = req.body
+        const newMovie = await Movie.create({
+            image,
+            title,
+            createdDate,
+            rating,
+        })
+
+        res.status(200).send(newMovie)
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.put("/", verifyToken, async (req,res,next) =>{
+
+    try {
+        
+        const {movieId, image, title, createdDate, rating} = req.body
+
+        const findMovie = await Movie.findOne({
+            where:{
+                id: movieId
+            }
+        })
+
+        if(findMovie){
+            await Movie.update({
+            image,
+            title,
+            createdDate,
+            rating,
+            },
+            {
+                where:{
+                    id: movieId
+                }
+            })
+            res.status(200).send("Película o serie modificada correctamente")
+        }else{
+            res.status(400).send("Película o serie no encontrada.")
+        }
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+router.delete("/", verifyToken, async (req,res,next) =>{
+
+    try {
+        
+        const {movieId} = req.body
+        
+        const findMovie = await Movie.findOne({
+            where:{
+                id: movieId
+            }
+        })
+
+        if(findMovie){
+            await Movie.destroy({
+                where:{
+                    id: movieId
+                }
+            })
+            res.status(200).send("Película o serie eliminada correctamente.")
+        } else {
+            res.status(400).send("Película o serie no encontrada")
+        }
+
+    } catch (error) {
+        next(error)
+    }
+})
+
 
 
 module.exports = router;
